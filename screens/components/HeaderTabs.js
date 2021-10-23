afa6dd9aa04da6829d7a9662a565fe51228874a2
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-23 15:31:51 -0700

    creating delivery and pick up button:

--- screens/components/HeaderTabs.js
+++ screens/components/HeaderTabs.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native'
 
 export default function HeaderTabs() {
     return (
-        <View>
-            <Text>Header Tabs</Text>
+        <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+            <HeaderButton text='delivery' />
+            <HeaderButton text='Pickup' />
         </View>
     )
 }
+
+const HeaderButton = (props) => (
+    <TouchableOpacity style={{
+        backgroundColor: 'black',
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30
+    }}
+    >
+        <Text style={{ color: 'white', fontSize: 15, fontWeight: '900' }}>
+            {props.text}
+        </Text>
+    </TouchableOpacity>
+);
