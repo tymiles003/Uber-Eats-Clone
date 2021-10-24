316bea854937b850c995eafaaa1a56c34142fd9a
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-24 10:57:52 -0700

    editting search bar

--- screens/components/SearchBar.js
+++ screens/components/SearchBar.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import React from "react";
+import { View, Text } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default function SearchBar() {
-    return (
-        <View style={{marginTop: 15, flexDirection: 'row'}}>
-            <GooglePlacesAutocomplete 
-            placeholder='Search'
-            />
-
-        </View>
-    )
+  return (
+    <View style={{ marginTop: 15, flexDirection: "row" }}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        styles={{
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            marginTop: 7,
+          },
+        }}
+      />
+    </View>
+  );
 }
