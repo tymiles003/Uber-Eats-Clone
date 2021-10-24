43d5ef46f289d2ba58e4fc52a9cdad872cdf1c36
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-24 13:52:02 -0700

    created loop for categories

--- components/Categories.js
+++ components/Categories.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 
 const items = [
   {
-    image: require("../../assets/images/shopping-bag.png"),
+    image: require("../assets/images/shopping-bag.png"),
     text: "Pick-up",
   },
   {
-    image: require("../../assets/images/soft-drink.png"),
+    image: require("../assets/images/soft-drink.png"),
     text: "Soft Drinks",
   },
   {
-    image: require("../../assets/images/bread.png"),
+    image: require("../assets/images/bread.png"),
     text: "Bakery Items",
   },
   {
-    image: require("../../assets/images/fast-food.png"),
+    image: require("../assets/images/fast-food.png"),
     text: "Fast Food",
   },
   {
-    image: require("../../assets/images/deals.png"),
+    image: require("../assets/images/deals.png"),
     text: "Deals",
   },
   {
@@ -30,8 +30,22 @@ const items = [
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      {items.map((item, index) => (
+        <View style={{ alignItems: "center", marginRight: 30 }}>
+          <Image
+            source={items[0].image}
+            style={{
+              width: 50,
+              height: 40,
+              resizeMode: "contain",
+            }}
+          />
+          <Text style={{ fontSize: 13, fontWeight: "900" }}>
+            {items[0].text}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
