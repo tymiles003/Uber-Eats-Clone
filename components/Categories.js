0e9d9dfa868cccfaeb173db66cf98c654bf70222
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-24 13:54:32 -0700

    done with categories

--- components/Categories.js
+++ components/Categories.js
@@ -30,22 +30,29 @@ const items = [
 
 export default function Categories() {
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {items.map((item, index) => (
-        <View style={{ alignItems: "center", marginRight: 30 }}>
-          <Image
-            source={items[0].image}
-            style={{
-              width: 50,
-              height: 40,
-              resizeMode: "contain",
-            }}
-          />
-          <Text style={{ fontSize: 13, fontWeight: "900" }}>
-            {items[0].text}
-          </Text>
-        </View>
-      ))}
-    </ScrollView>
+    <View
+      style={{
+        marginTop: 5,
+        backgroundColor: "white",
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, index) => (
+          <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+            <Image
+              source={item.image}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
   );
 }
