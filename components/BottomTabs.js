aae43b7d9ab39087576e028be728bb8789a6e1ed
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-25 21:46:45 -0700

    imported fontawesome5 and created home icon

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,10 +1,32 @@
 import React from "react";
 import { View, Text } from "react-native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 export default function BottomTabs() {
   return (
-    <View>
+    <View
+      style={{
+        flexDirection: "row",
+        margin: 10,
+        marginHorizontal: 30,
+        justifyContent: "space-between",
+      }}
+    >
+      <Icon icon="home" text="Home" />
+      <Text>Bottom</Text>
+      <Text>Bottom</Text>
       <Text>Bottom</Text>
     </View>
   );
 }
+
+const Icon = (props) => (
+  <FontAwesome5
+    name={props.icon}
+    size={25}
+    style={{
+      marginBottom: 3,
+      alignSelf: "center",
+    }}
+  />
+);
