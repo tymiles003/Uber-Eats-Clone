ebfaef49384f114da343b59e5e79abfd9d95e13f
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-11-17 21:33:43 -0800

    added conditional

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -23,38 +23,44 @@ export default function ViewCart() {
   });
   console.log(totalUSD);
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-        position: "absolute",
-        bottom: 10,
-        zIndex: 999,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          width: "100%",
-        }}
-      >
-        <TouchableOpacity
+    <>
+      {total ? (
+        <View
           style={{
-            marginTop: 20,
-            backgroundColor: "black",
+            flex: 1,
             alignItems: "center",
-            padding: 13,
-            borderRadius: 30,
-            width: 300,
-            position: "relative",
+            justifyContent: "center",
+            flexDirection: "row",
+            position: "absolute",
+            bottom: 10,
+            zIndex: 999,
           }}
         >
-          <Text style={{ color: "white", fontSize: 20 }}>View Cart</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                marginTop: 20,
+                backgroundColor: "black",
+                alignItems: "center",
+                padding: 13,
+                borderRadius: 30,
+                width: 300,
+                position: "relative",
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 20 }}>View Cart</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
